a52a851b016f32859f9cddafd61dfef8dea39283
guillaume <gsautie@qti.qualcomm.com>
2020-04-27 15:45:17 +0200

    add cage noire script

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# la-cage-noire
+# La Cage Noire
+
 Student project Sorbonne Paris 1 2020 objects intéractifs
+
+## Installation
+
+To install with [`conda`](https://docs.conda.io/projects/conda/en/latest/user-guide/install/):
+
+```bash
+conda create --name cage python=3.7
+conda activate cage
+conda install -c conda-forge --file requirements-conda.txt
+```
+
+## Run
+
+You will need three things:
+
+* a microphone
+* a separate (bluetooth) speaker
+* a headset
+* the files with the testimonies (do not rename them, put them in same folder as script.py)
+
+To run the project (after running installation instructions above):
+
+```bash
+conda activate cage
+python script.py
+```
---
+++ script.py
@@ -0,0 +1,65 @@
+
+import warnings
+from collections import defaultdict
+from time import sleep, time
+
+import librosa
+import numpy as np
+import readchar
+import sounddevice as sd
+
+warnings.filterwarnings("ignore")
+
+PEOPLES = ['camille', 'indira', 'nao']
+
+
+def main(debug=False):
+    filepaths = {people: f'testimony-{people}.m4a' for people in PEOPLES}
+    recordings, sample_rates = {}, {}
+
+    print('Loading the testimonies')
+    for people, filepath in filepaths.items():
+        start_time = time()
+        testimony, sample_rate = librosa.load(filepath)
+        recordings[people] = testimony
+        sample_rates[people] = sample_rate
+        print(f'Loaded testimony of {people} in {time() - start_time:.1f}s')
+        print(f'  - sample rate {sample_rate}Hz')
+        print(f'  - duration {len(testimony)/sample_rate:.1f}s ({len(testimony)/sample_rate/60:.1f}m)')
+
+    mapping = {people[:1]: (recordings[people], sample_rates[people]) for people in PEOPLES}
+    print('Entering loop...')
+    while True:
+        print('Awaiting key press...')
+        print('Press "c", "i" or "n" to listen to a testimony, "s" to quit program')
+        key = readchar.readkey()
+        if key in mapping:
+            sd.stop()
+            testimony, sample_rate = mapping[key]
+            recording = sd.playrec(testimony, samplerate=sample_rate, channels=1)[:, 0]
+            try:
+                sd.wait()
+            except KeyboardInterrupt:
+                sd.stop()
+                print('Detected movement, stopping play and recording')
+                continue
+            sleep(2)
+            print('Should be playing recording now')
+            sd.play(recording, samplerate=sample_rate, device=1)
+            try:
+                sd.wait()
+                print('Done waiting...')
+            except KeyboardInterrupt:
+                sd.stop()
+                print('Detected movement, stopping playback of recording.')
+                continue
+        elif key == 's':
+            print('Stopping.')
+            break
+        else:
+            sd.stop()
+            print(f'You pressed {key}, only key "c", "i" or "n" will trigger sound.')
+
+
+if __name__ == '__main__':
+    main()
